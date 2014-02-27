b1fb6eaa38a07980d7a64062bdd44c2ad88481ed
Victor ALBERT <albert@novius.fr>
2014-02-27 18:06:03 +0100

    change default image and add information in appdesk

--- config/common/faq.config.php
+++ config/common/faq.config.php
@@ -5,6 +5,18 @@ return array(
         'faq_title' => array(
             'title' => __('Title'),
         ),
+        'questions' => array(
+            'title' => __('Number of Q&As'),
+            'value' => function($item) {
+                return count($item->questions);
+            },
+        ),
+        'category' => array(
+            'title' => __('Category'),
+            'value' => function($item) {
+                return !empty($item->faq_cate_id) ? $item->category->cate_title : '';
+            },
+        ),
         'context' => true,
         'publication_status' => true,
     ),
